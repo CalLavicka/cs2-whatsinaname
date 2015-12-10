2d6b8a18c34e4ed07a37ec76d3ee01d75db9e735
CalLavicka <cal.lavicka@gmail.com>
2015-12-10 15:12:15 -0500

    first commit

---
+++ src/WhatsInAName.java
@@ -0,0 +1,61 @@
+
+public class WhatsInAName {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	// Grace
+	static int consonantFrequency(char[] array, char consonant) {
+		return 0;
+	}
+	
+	static boolean lastnameHyphen(char[] array) {
+		return false;
+	}
+	
+	// Jocelyn
+	static void convertLowercase(char[] array) {
+		
+	}
+	
+	static int numberVowels(char[] array) {
+		return 0;
+	}
+	
+	// Priya
+	static void convertUppercase(char[] array) {
+		
+	}
+	
+	static void randomizeName(char[] array) {
+		
+	}
+	
+	static char[] returnMiddleName(char[] array) {
+		return null;
+	}
+	
+	// Sara
+	static boolean firstNamePalindrome(char[] array) {
+		return false;
+	}
+	
+	static char[] returnFirstName(char[] array) {
+		return null;
+	}
+	
+	// Jason
+	static char[] initialsFromName(char[] array) {
+		return null;
+	}
+	
+	static char[] returnLastName(char[] array) {
+		return null;
+	}
+	
+	static void reverseAndPrintArray(char[] array) {
+		
+	}
+}
