0136167656a59c17fd6d9fd5c705e839015752dc
watermelonheart <c18gi@dalton.org>
2015-12-14 11:40:43 -0500

    Hey ya'll it's Watermelon heart. Updated da git!!

--- src/WhatsInAName.java
+++ src/WhatsInAName.java
@@ -1,61 +1,76 @@
 
 public class WhatsInAName {
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	// Grace
 	static int consonantFrequency(char[] array, char consonant) {
-		return 0;
+		int frequency = 0;
+		for (int i = 0; i < array.length; i++) {
+			if(array[i] == consonant) {
+				frequency ++;
+
+			}
+		}
+
+		return frequency;
 	}
-	
+
 	static boolean lastnameHyphen(char[] array) {
+		char hyphen = '-';
+		for (int i = array.length -1; array[i] != ' '; i--) {
+			if(array[i] == hyphen) {
+				return true;
+			}
+
+		}
 		return false;
 	}
-	
+
 	// Jocelyn
 	static void convertLowercase(char[] array) {
-		
+
 	}
-	
+
 	static int numberVowels(char[] array) {
 		return 0;
 	}
-	
+
 	// Priya
 	static void convertUppercase(char[] array) {
-		
+
 	}
-	
+
 	static void randomizeName(char[] array) {
-		
+
 	}
-	
+
 	static char[] returnMiddleName(char[] array) {
 		return null;
 	}
-	
+
 	// Sara
 	static boolean firstNamePalindrome(char[] array) {
 		return false;
 	}
-	
+
 	static char[] returnFirstName(char[] array) {
 		return null;
 	}
-	
+
 	// Jason
 	static char[] initialsFromName(char[] array) {
 		return null;
 	}
-	
+
 	static char[] returnLastName(char[] array) {
 		return null;
 	}
-	
+
 	static void reverseAndPrintArray(char[] array) {
-		
+
 	}
 }
